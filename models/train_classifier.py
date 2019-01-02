74dae8202a94452a9e65f1a4a7ffbb91c0e98896
wanghuan <wanghuan2918@163.com>
2019-01-02 14:46:36 +0800

    Update train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -61,11 +61,11 @@ def build_model():
                          ('clf', MultiOutputClassifier(RandomForestClassifier()))])
     # set the parameters of the model
     parameters = {'vect__ngram_range': ((1, 1), (1, 2)),
-                 # 'tfidf__use_idf': (True, False),
-                 # 'clf__estimator__n_estimators':[50, 100, 200],
-                  #'clf__estimator__max_depth':[50, 500, 1000, 5000],
-                  #'clf__estimator__max_features': [2000, 5000, 10000, 20000],
-                  'clf__estimator__min_samples_split':[3, 5, 9]}
+                 'tfidf__use_idf': (True, False),
+                 'clf__estimator__n_estimators':[50, 100, 200],
+                 'clf__estimator__max_depth':[50, 500, 1000, 5000],
+                 'clf__estimator__max_features': [2000, 5000, 10000, 20000],
+                 'clf__estimator__min_samples_split':[3, 5, 9]}
     cv = GridSearchCV(pipeline, param_grid = parameters)
     return cv
 
